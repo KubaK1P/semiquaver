b3f98804e4212f0ca47d760023812fd069a991be
KubaK1P <1p22kus@gmail.com>
2024-10-18 11:58:26 +0200

    restrukturyzacja

---
+++ sklep_muzyczny/html/home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Semiquaver - home page</title>
+    <!-- <link rel="stylesheet" href="../css/main.css"> -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="container">
+       <?php
+       include "../components/header.shtml";
+       ?>
+        <main class="">
+            <header class="">
+                <!-- position: absolute; maybe -->
+                <h2>Semiquaver</h2>
+                <p>Your only choice about music equipment</p>
+                <div class="">
+                    <button>Click</button>
+                    <button>Click</button>
+                </div>
+            </header>
+        </main>
+        <section class="">
+            <div class="">
+                <!-- background image -->
+                </div>            
+        </section>
+        <aside class="">
+            <header class="">
+                <h2>Opinie</h2>
+                <h3>Są</h3>
+            </header>
+            <section class="">
+                <article class="">Opinia</article>
+                <article class="">Opinia</article>
+                <article class="">Opinia</article>
+            </section>
+        </aside>
+        <?php
+        include "../components/footer.shtml";
+        ?>
+    </div>
+</body>
+</html>
---
+++ sklep_muzyczny/html/store.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Semiquaver - store</title>
+    <!-- <link rel="stylesheet" href="../css/main.css"> -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="container">
+       <?php
+       include "../components/header.shtml";
+       ?>
+        <main class="">
+            
+        </main>
+        <?php
+        include "../components/footer.shtml";
+        ?>
+    </div>
+</body>
+</html>
