5a9687fc2eb4704d33b6adfe9649788966431e6f
KubaK1P <1p22kus@gmail.com>
2024-10-19 15:51:03 +0200

    achievement: it's hard to code something that's this ugly in tailwind

--- sklep_muzyczny/html/home.php
+++ sklep_muzyczny/html/home.php
@@ -14,9 +14,9 @@
        include "../components/header.shtml";
        ?>
         <main class="h-[89vh] relative p-6" id="main">
-            <header class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <header class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white font-bold">
                 <!-- position: absolute; maybe -->
-                <h2>Semiquaver</h2>
+                <h2 class="text-6xl tracking-wide">Semiquaver</h2>
                 <p>Your only choice about music equipment</p>
                 <div class="">
                     <button>Click</button>
@@ -25,38 +25,37 @@
             </header>
         </main>
         <section class="relative h-[89vh] p-6" id="section">   
-        <header class="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-                <!-- position: absolute; maybe -->
-                <h2>SemiGood Guitars</h2>
-                <p>Check out our guitar creator</p>
-                <div class="">
-                    <button>Click</button>
-                    <button>Click</button>
-                </div>
+            <header class="absolute top-[50%] left-[70%] translate-x-[-50%] translate-y-[-50%] text-white font-bold">
+                    <!-- position: absolute; maybe -->
+                    <h2 class="text-5xl tracking-wide">SemiGood Guitars</h2>
+                    <p>Check out our guitar creator</p>
+                    <div class="">
+                        <a href="#" class="text-sky-300">Our creator</a>
+                    </div>
             </header>      
         </section>
-        <aside class="h-[40vh] p-6">
-            <header class="p-2 m-2">
+        <aside class="h-[50vh] p-6 bg-gradient-to-r from-white to-sky-200">
+            <header class="p-2 m-4 text-3xl font-semibold">
                 <h2 class="text-center">Our Customers' words</h2>
             </header>
             <section class="flex justify-evenly">
-                <article class="p-2">
+                <article class="transition duration-350 basis-[25%] block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-sky-200  dark:border-gray-700">
                     <header> 
-                        <h3>Clark</h3>
+                        <h3 class="mb-2 text-2xl font-bold tracking-tight">Clark</h3>
                     </header>
-                    <p>The shop worked fine </p>
+                    <p class="font-normal">The shop worked fine </p>
                 </article>
-                <article class="">
+                <article class="transition duration-350 basis-[25%] block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-sky-200  dark:border-gray-700">
                     <header> 
-                        <h3>Clark</h3>
+                        <h3 class="mb-2 text-2xl font-bold tracking-tight">Clark</h3>
                     </header>
-                    <p>The shop worked fine </p>
+                    <p class="font-normal">The shop worked fine </p>
                 </article>
-                <article class="">
+                <article class="transition duration-350 basis-[25%] block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-sky-200  dark:border-gray-700">
                     <header> 
-                        <h3>Clark</h3>
+                        <h3 class="mb-2 text-2xl font-bold tracking-tight">Clark</h3>
                     </header>
-                    <p>The shop worked fine </p>
+                    <p class="font-normal">The shop worked fine </p>
                 </article>
             </section>
         </aside>
