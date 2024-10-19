7a152d2205478bbb3f3b204a1ff1d84090af0f38
KubaK1P <1p22kus@gmail.com>
2024-10-19 21:18:23 +0200

    feat: "style" the store page

--- sklep_muzyczny/html/store.php
+++ sklep_muzyczny/html/store.php
@@ -5,16 +5,156 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Semiquaver - store</title>
     <!-- <link rel="stylesheet" href="../css/main.css"> -->
+     <link rel="stylesheet" href="../css/background_image.css">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <div class="">
-       <?php
-       include "../components/header.shtml";
-       ?>
-        <main class="">
-            
-        </main>
+        <?php
+        include "../components/header.shtml";
+        ?>
+        <header class="relative h-[95vh] text-[600%] text-white font-semibold p-6" id="store">
+            <div class="flex flex-col items-center absolute top-[77%] left-[50%] translate-x-[-50%] translate-y-[-70%]">
+                <h1 class="">Store</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+	                <path fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.35" d="m6 6l6 6l6-6M6 12l6 6l6-6" />
+                </svg>
+            </div>
+        </header>
+        <div class="flex min-h-[60vh] p-6 bg-gradient-to-r from-white to-sky-200">
+            <aside class="flex flex-col basis-[35%] p-2">
+                <section>
+                    <header class="text-3xl text-semibold p-2">
+                        <h2>Categories</h2>
+                    </header>
+                    <ul>
+                        <li><a href="#">Category</a></li>
+                        <li><a href="#">Category</a></li>
+                        <li><a href="#">Category</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <header class="text-3xl text-semibold p-2">
+                        <h2>Filters</h2>
+                    </header>
+                    <ul>
+                        <li><a href="#">Filter</a></li>
+                        <li><a href="#">Filter</a></li>
+                        <li><a href="#">Filter</a></li>
+                    </ul>
+                </section>
+            </aside>
+            <main class="basis-[65%] p-2">
+                <header class="text-4xl text-bold mb-6">
+                    <h2 class="w-1/2 shadow-md rounded-lg border border-gray-300 p-2 pl-4">Products</h2>
+                </header>
+                <div class="flex flex-wrap justify-between gap-6 pl-10 pr-10" id="products">
+                    <div class="basis-[30%] bg-white border border-gray-200 rounded-lg shadow">
+                        <a href="#">
+                            <img class="rounded-t-lg" src="../img/product-example.jpg" alt="product image" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Product name</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 ">Product info</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-800 bg-gradient-to-r from-white to-sky-200 rounded-lg shadow">
+                                Read more
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="basis-[30%] bg-white border border-gray-200 rounded-lg shadow">
+                        <a href="#">
+                            <img class="rounded-t-lg" src="../img/product-example.jpg" alt="product image" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Product name</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 ">Product info</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-800 bg-gradient-to-r from-white to-sky-200 rounded-lg shadow">
+                                Read more
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="basis-[30%] bg-white border border-gray-200 rounded-lg shadow">
+                        <a href="#">
+                            <img class="rounded-t-lg" src="../img/product-example.jpg" alt="product image" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Product name</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 ">Product info</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-800 bg-gradient-to-r from-white to-sky-200 rounded-lg shadow">
+                                Read more
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="basis-[30%] bg-white border border-gray-200 rounded-lg shadow">
+                        <a href="#">
+                            <img class="rounded-t-lg" src="../img/product-example.jpg" alt="product image" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Product name</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 ">Product info</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-800 bg-gradient-to-r from-white to-sky-200 rounded-lg shadow">
+                                Read more
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="basis-[30%] bg-white border border-gray-200 rounded-lg shadow">
+                        <a href="#">
+                            <img class="rounded-t-lg" src="../img/product-example.jpg" alt="product image" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Product name</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 ">Product info</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-800 bg-gradient-to-r from-white to-sky-200 rounded-lg shadow">
+                                Read more
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="basis-[30%] bg-white border border-gray-200 rounded-lg shadow">
+                        <a href="#">
+                            <img class="rounded-t-lg" src="../img/product-example.jpg" alt="product image" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Product name</h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 ">Product info</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-800 bg-gradient-to-r from-white to-sky-200 rounded-lg shadow">
+                                Read more
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+            </main>
+        </div>
         <?php
         include "../components/footer.shtml";
         ?>
