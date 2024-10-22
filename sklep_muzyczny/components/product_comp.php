ac2c4adc9679b8d3da92faa4a5a0852d34106089
KubaK1P <1p22kus@gmail.com>
2024-10-22 20:49:07 +0200

    feat: add mysql_connect.php, incomplete

--- sklep_muzyczny/components/product_comp.php
+++ sklep_muzyczny/components/product_comp.php
@@ -8,7 +8,8 @@
         </a>
         <p class="mb-3 font-normal text-gray-700 ">Product info</p>
         <div class="flex justify-between items-center">
-        <a href="product.php" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-800 bg-gradient-to-r from-white to-sky-200 rounded-lg shadow">
+            <!-- Get atribute with php -->
+        <a href="./product.php?id=0" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-gray-800 bg-gradient-to-r from-white to-sky-200 rounded-lg shadow">
             Product subpage
             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
---
+++ sklep_muzyczny/scripts/mysql_connect.php
@@ -0,0 +1,3 @@
+<?php
+// connect to the database later (23.10?)
+// Include (require) this in the right files
