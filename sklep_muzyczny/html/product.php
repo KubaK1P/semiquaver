3717e4a4fc71cb4fa9712b84118e42ece6b8b258
Jakub Kus <1p22kus@gmail.com>
2024-10-24 20:26:36 +0200

    feat: product.php data from db and layout

--- sklep_muzyczny/html/product.php
+++ sklep_muzyczny/html/product.php
@@ -5,9 +5,9 @@ require("../scripts/mysql_connect.php");
 $conn = connect();
 $productIdGet = $_GET["id"];
 
-$result = mysqli_query($conn, "SELECT produkt.Nazwa_produktu, produkt.Opis_produktu, produkt.Cena_jednostkowa, produkt.Zdjecie_produktu FROM produkt INNER JOIN kategoria_produktu ON kategoria_produktu.Id_kategorii_produktu = produkt.Id_kategorii_produktu WHERE produkt.Id_produktu = " . $productIdGet . ";");
-
-$product = mysqli_fetch_assoc($result);
+$resultMain = mysqli_query($conn, "SELECT produkt.Nazwa_produktu, produkt.Opis_produktu, produkt.Cena_jednostkowa, produkt.Zdjecie_produktu, kategoria_produktu.Nazwa_kategorii_produktu FROM produkt INNER JOIN kategoria_produktu ON kategoria_produktu.Id_kategorii_produktu = produkt.Id_kategorii_produktu WHERE produkt.Id_produktu = " . $productIdGet . ";");
+// $resultAside = mysqli_query($conn, "");
+$product = mysqli_fetch_assoc($resultMain);
 ?>
 <!-- Działa skrypciik -->
 <!DOCTYPE html>
@@ -29,8 +29,27 @@ $product = mysqli_fetch_assoc($result);
         <?php
         include "../components/header.shtml";
         ?>
-        <main>
-
+        <main class="h-[75vh] p-6 flex flex-col justify-center">
+            <div class="flex justify-around">
+                <header class="max-w-[40%] p-6"> 
+                    <h1 class="mb-4 tracking-wide text-4xl text-gray-800 font-bold"><?php echo $product["Nazwa_produktu"];?></h1>
+                    <a href="store.php?category=1" class="text-2xl text-gray-500 font-semibold"><?php echo $product["Nazwa_kategorii_produktu"];?></a>
+                    <p class="mt-3 mb-4 text-lg text-gray-600 font-medium"><?php echo $product["Opis_produktu"];?></p>
+                    <div class="flex justify-between items-center">
+                            <!-- Get atribute with php -->
+                        <a href="./product.php$id=<?php echo $productIdGet;?>" class="inline-flex items-center px-3 py-2 text-xl font-semibold text-center text-gray-800 bg-gradient-to-r from-white to-sky-200 rounded-lg shadow">
+                            Add to cart
+                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                        </a>
+                        <p class="text-xl text-gray-800 p-2 font-semibold"><?php echo $product["Cena_jednostkowa"];?> zł</p>
+                    </div>
+                </header>
+                <div class="max-w-[40%] min-w-[35%] flex justify-center">
+                    <img class="rounded-t-lg max-h-[40vh]" src="<?php echo $product["Zdjecie_produktu"]; ?> " alt="product image" />
+                </div>
+            </div>
         </main>
         <aside>
 
@@ -41,3 +60,7 @@ $product = mysqli_fetch_assoc($result);
     </div>
 </body>
 </html>
+
+<?php
+mysqli_close($conn);
+?>
