ca2de4fe65dfd4ffbfde09aaa26e4fe83c03a6e8
Jakub Kus <1p22kus@gmail.com>
2024-10-26 21:05:06 +0200

    feat: add product search

--- sklep_muzyczny/components/product_comp_list.php
+++ sklep_muzyczny/components/product_comp_list.php
@@ -3,14 +3,20 @@
 require("../scripts/mysql_connect.php");
 include "../components/product_comp.php";
 
-$conn = connect();
+function show_products($query)
+{
+    $conn = connect();
 
-$result = mysqli_query($conn ,"SELECT produkt.Id_produktu, produkt.Nazwa_produktu, produkt.Opis_produktu, produkt.Cena_jednostkowa, produkt.Zdjecie_produktu, kategoria_produktu.Nazwa_kategorii_produktu FROM produkt INNER JOIN kategoria_produktu ON produkt.Id_kategorii_produktu = kategoria_produktu.Id_kategorii_produktu");
+    $result = mysqli_execute_query($conn, $query);
 
-$products = mysqli_fetch_all($result, MYSQLI_ASSOC);
-// echo var_dump($products);
-foreach ($products as $product) {
-    product($product["Id_produktu"], $product["Nazwa_produktu"], $product["Cena_jednostkowa"], $product["Zdjecie_produktu"], $product["Nazwa_kategorii_produktu"], 30);
+    $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    // $products_length = count($products);
+
+    // echo $products_length;
+    foreach ($products as $product) {
+        product($product["Id_produktu"], $product["Nazwa_produktu"], $product["Cena_jednostkowa"], $product["Zdjecie_produktu"], $product["Nazwa_kategorii_produktu"], 30);
+    }
+
+    mysqli_close($conn);
+    // return $products_length;
 }
-
-mysqli_close($conn);
--- sklep_muzyczny/html/store.php
+++ sklep_muzyczny/html/store.php
@@ -1,13 +1,26 @@
+<?php
+require("../scripts/search_query.php");
+
+if (!(isset($_POST["search_term"]))) {
+    $query = "SELECT produkt.Id_produktu, produkt.Nazwa_produktu, produkt.Opis_produktu, produkt.Cena_jednostkowa, produkt.Zdjecie_produktu, kategoria_produktu.Nazwa_kategorii_produktu FROM produkt INNER JOIN kategoria_produktu ON produkt.Id_kategorii_produktu = kategoria_produktu.Id_kategorii_produktu";
+} else {
+    $search_term = $_POST["search_term"];
+    $query = get_query($search_term);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Semiquaver - store</title>
     <!-- <link rel="stylesheet" href="../css/main.css"> -->
-     <link rel="stylesheet" href="../css/background_image.css">
+    <link rel="stylesheet" href="../css/background_image.css">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
     <div class="">
         <?php
@@ -15,9 +28,9 @@
         ?>
         <header class="relative h-[100vh] text-[600%] text-white font-semibold p-6" id="store">
             <div class="flex flex-col items-center absolute top-[77%] left-[50%] translate-x-[-50%] translate-y-[-70%]">
-                <h1 class="">Store</h1>
+                <h1 class="" id="searchResult">Store</h1>
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-	                <path fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.35" d="m6 6l6 6l6-6M6 12l6 6l6-6" />
+                    <path fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.35" d="m6 6l6 6l6-6M6 12l6 6l6-6" />
                 </svg>
             </div>
         </header>
@@ -45,13 +58,26 @@
                 </section>
             </aside>
             <main class="basis-[65%] p-2">
-                <header class="text-4xl text-bold mb-6">
-                    <h2 class="w-1/2 shadow-md rounded-lg border border-gray-300 p-2 pl-4">Products</h2>
+                <header class="flex text-4xl text-bold mb-6">
+                    <h2 class="text-5xl basis-1/2 p-2 pl-4">Products</h2>
+                    <form action="./store.php#searchResult" method="post" class="flex basis-1/2 gap-4 rounded-md border-2 border-sky-500 overflow-hidden mx-auto">
+                        <input type="text" placeholder="Search products" name="search_term"
+                            class="w-full outline-none bg-white text-gray-700 text-lg px-4 py-3" />
+                        <button value="" type='submit' class="flex items-center justify-center bg-sky-400 px-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px" class="fill-white">
+                                <path
+                                    d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z">
+                                </path>
+                            </svg>
+                        </button>
+                    </form>
                 </header>
-                <div class="flex flex-wrap justify-between gap-6 pl-10 pr-10" id="products">
-                <?php
+                <div class="flex flex-wrap gap-8 pl-10 pr-10" id="products">
+                    <?php
                     include "../components/product_comp_list.php";
-                ?>
+
+                    show_products($query);
+                    ?>
                 </div>
             </main>
         </div>
@@ -60,4 +86,5 @@
         ?>
     </div>
 </body>
+
 </html>
