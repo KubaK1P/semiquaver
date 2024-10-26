59a91fdb908c75c6a10d68d45eec5bff90137abc
Jakub Kus <1p22kus@gmail.com>
2024-10-26 19:35:27 +0200

    feat: add search_query.php

---
+++ sklep_muzyczny/scripts/search_query.php
@@ -0,0 +1,8 @@
+<?php
+
+$term = $_POST["search_term"];
+
+function get_query($term) {
+    $query = "SELECT produkt.Id_produktu, produkt.Nazwa_produktu, produkt.Opis_produktu, produkt.Cena_jednostkowa, produkt.Zdjecie_produktu, kategoria_produktu.Nazwa_kategorii_produktu FROM produkt INNER JOIN kategoria_produktu ON produkt.Id_kategorii_produktu = kategoria_produktu.Id_kategorii_produktu WHERE produkt.Nazwa_produktu LIKE \"%" . $term . "%\" OR produkt.Nazwa_kategorii_produktu LIKE \"%" . $term . "%\" ORDER BY produkt.Nazwa_produktu ASC;";
+    return $query;
+}
