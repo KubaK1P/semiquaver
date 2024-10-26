bd29285c2e0e9babb2e0af9d6d2c28ac0db60b37
KubaK1P <1p22kus@gmail.com>
2024-10-26 12:18:03 +0200

    fix: style reviews section in home.php

--- sklep_muzyczny/html/home.php
+++ sklep_muzyczny/html/home.php
@@ -33,7 +33,7 @@
                     </div>
             </header>      
         </section>
-        <aside class="h-[50vh] p-6">
+        <aside class="min-h-[65vh] p-6">
             <header class="p-2 m-4 text-3xl font-semibold">
                 <h2 class="text-center">Our Customers' words</h2>
             </header>
@@ -42,19 +42,23 @@
                     <header> 
                         <h3 class="mb-2 text-2xl font-bold tracking-tight">Clark</h3>
                     </header>
-                    <p class="font-normal">The shop worked fine </p>
+                    <p class="font-normal h-3/4 text-gray-700">Having had the opportunity to test out [product name], I must say that I am thoroughly impressed. The [specific feature] really stood out to me, offering a level of convenience and efficiency that is unparalleled in the market. Additionally, the [another feature] surpassed my expectations, enhancing the overall user experience significantly. 
+                    </p>
+                    <span class="text-lg font-semibold text-gray-800">5/5</span>
                 </article>
                 <article class="transition duration-350 basis-[25%] block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-sky-200  dark:border-gray-700">
                     <header> 
-                        <h3 class="mb-2 text-2xl font-bold tracking-tight">Clark</h3>
+                        <h3 class="mb-2 text-2xl font-bold tracking-tight">John</h3>
                     </header>
-                    <p class="font-normal">The shop worked fine </p>
+                    <p class="font-normal h-3/4 text-gray-700">For [target audience], this product is a game-changer. Whether you are a seasoned professional or a tech-savvy individual looking for a reliable [product category], [product name] ticks all the boxes. Its durability, performance, and user-friendly interface make it a must-have in today's fast-paced world.</p>
+                    <span class="text-lg font-semibold text-gray-800">5/5</span>
                 </article>
                 <article class="transition duration-350 basis-[25%] block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-sky-200  dark:border-gray-700">
                     <header> 
-                        <h3 class="mb-2 text-2xl font-bold tracking-tight">Clark</h3>
+                        <h3 class="mb-2 text-2xl font-bold tracking-tight">M3dzia</h3>
                     </header>
-                    <p class="font-normal">The shop worked fine </p>
+                    <p class="font-normal h-3/4 text-gray-700">In conclusion, the [product name] is a top-notch [product category] that delivers on its promises. With its cutting-edge technology and superior design, it is sure to enhance the lives of its users in more ways than one. I highly recommend [product name] to anyone in search of a premium [product category] that exceeds expectations.</p>
+                    <span class="text-lg font-semibold text-gray-800">5/5</span>                    
                 </article>
             </section>
         </aside>
