a2eb874a429e136b0004e58186806341f741f307
KubaK1P <1p22kus@gmail.com>
2024-10-30 09:27:47 +0100

    patch: added content validation
    Co-authored-by: 1p22geo <1p22geodecki@gmail.com>
    Signed-off-by: 1p22geo <1p22geodecki@gmail.com>

--- sklep_muzyczny/scripts/search_query.php
+++ sklep_muzyczny/scripts/search_query.php
@@ -2,7 +2,14 @@
 require("../scripts/mysql_connect.php");
 
 function get_products($term) {
-    $term = "%" . $term . "%";
+    $char_whitelist = str_split("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM ");
+    $new_term = "";
+    foreach(str_split($term) as $char){
+        if(array_search($char, $char_whitelist)){
+            $new_term .= $char;
+        }
+    }
+    $term = "%" . $new_term . "%";
 
     $conn = connect();
     $query = "SELECT produkt.Id_produktu, produkt.Nazwa_produktu, produkt.Opis_produktu, produkt.Cena_jednostkowa, produkt.Zdjecie_produktu, kategoria_produktu.Nazwa_kategorii_produktu FROM produkt INNER JOIN kategoria_produktu ON produkt.Id_kategorii_produktu = kategoria_produktu.Id_kategorii_produktu WHERE produkt.Nazwa_produktu LIKE ? OR kategoria_produktu.Nazwa_kategorii_produktu LIKE ? ORDER BY produkt.Nazwa_produktu ASC;";
