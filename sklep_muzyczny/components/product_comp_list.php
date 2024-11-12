8b147cda28a96bf176f21122c2d13f8ac7c091fa
Jakub Kus <113033886+KubaK1P@users.noreply.github.com>
2024-11-12 09:09:56 +0100

    Update product_comp_list.php

--- sklep_muzyczny/components/product_comp_list.php
+++ sklep_muzyczny/components/product_comp_list.php
@@ -11,6 +11,5 @@ function show_products($term, $width, $exclude)
     foreach ($products as $product) {
         product($product["Id_produktu"], $product["Nazwa_produktu"], $product["Cena_jednostkowa"], $product["Zdjecie_produktu"], $product["Nazwa_kategorii_produktu"], $width);
     }
-    echo $productsLength;
     return $productsLength;
 }
